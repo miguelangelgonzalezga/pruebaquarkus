3f243c1d659d4990f213433c103725f062dc516b
miguelangelgonzalezga <miguelangel.gonzalezga@viewnext.com>
2025-04-30 16:16:28 +0200

    Add files via upload

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,38 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+	
+	// This is the main Micrometer class to register custom metrics
+    private final MeterRegistry registry;
+    // The data to monitor
+    private AtomicInteger currentMemory;
+    // Injects the Micrometer registry
+    GreetingResource(MeterRegistry registry) {
+        this.registry = registry;
+        // Registers this metric under current.memory name, initializing the counter to 0
+        currentMemory = this.registry.gauge("current.memory", Tags.empty(), new AtomicInteger(0));
+    }
+    // Creates an endpoint to modify the observed variable 
+    @GET
+    @Path("/consume/{amount}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Integer consume(@PathParam("amount") int mem) {
+        this.currentMemory.addAndGet(mem);
+        return this.currentMemory.get();
+    }
+}
